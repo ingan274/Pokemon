b7fef4092b6d41f031de32b130dbad8718df2645
ingan <isabelngan27@gmail.com>
2019-06-30 00:28:30 -0700

    set up initiation function

--- assets/js/pokemon.js
+++ assets/js/pokemon.js
@@ -1,63 +1,114 @@
 // Game controls:
 // 1. Select Pokemon
 // 2. Select Opponent
-// 3. When clicking on "attack," attach will double and my HP will go down with their attach
+// 3. When clicking on "attack," attach will increase and my HP will go down with their defense
 // 4. WIN - opponent will leave the screen 
 // 4A. Player selects new opponent
 // 5. LOSE - game is reset
 
 $(document).ready(function () {
-// 1. Select Pokemon
-// pokemon Array
+    // 1. Select Pokemon
+    // pokemon Array
 
-var pokemon = [
-    {
-        name:'charmander';
-        hp: 139;
-        attack: 2;
-        counterattack: 3;
-        img: '../assets/img/charmander.png'
-    },
-    {
-        name: 'dugtrio';
-        hp: 135;
-        attack: 10;
-        counterattack: 6;
-        img: '../assets/img/dugtrio.png'
-    },
-    {
-        name: 'eevee';
-        hp: 155;
-        attack: 5;
-        counterattack: 10;
-        img: '../assets/img/eve.png'
-    },
-    {
-        name: 'persian';
-        hp: 165;
-        attack: 10;
-        counterattack: 10;
-        img: '../assets/img/persian.png'
-    },
-    {
-        name: 'pikachu';
-        hp: 135;
-        attack: 15;
-        counterattack: 10;
-        img: '../assets/img/pikachu.png'
-    },
-    {
-        name: 'wailmer';
-        hp: 230;
-        attack: 12;
-        counterattack: 5;
-        img: '../assets/img/wailmer.png'
-    },
-]
-// 2. Select Opponent
-// 3. When clicking on "attack," attach will double and my HP will go down with their attach
-// 4. WIN - opponent will leave the screen 
-// 4A. Player selects new opponent
-// 5. LOSE - game is reset
+    var pokemonSet = [
+        {
+            name: 'Charmander',
+            ogHp: 70,
+            hp: 70,
+            attack: 20,
+            defense: 33,
+            img: 'assets/img/charmander.png'
+        },
+        {
+            name: 'Dugtrio',
+            ogHp: 90,
+            hp: 90,
+            attack: 40,
+            defense: 30,
+            img: 'assets/img/dugtrio.png'
+        },
+        {
+            name: 'Eevee',
+            ogHp: 60,
+            hp: 60,
+            attack: 25,
+            defense: 20,
+            img: 'assets/img/eve.png'
+        },
+        {
+            name: 'Persian',
+            ogHp: 100,
+            hp: 100,
+            attack: 30,
+            defense: 20,
+            img: 'assets/img/persian.png'
+        },
+        {
+            name: 'Pikachu',
+            ogHp: 70,
+            hp: 70,
+            attack: 30,
+            defense: 40,
+            img: 'assets/img/pikachu.png'
+        },
+        {
+            name: 'Wailmer',
+            ogHp: 130,
+            hp: 130,
+            attack: 70,
+            defense: 35,
+            img: 'assets/img/wailmer.png'
+        },
+    ]
+
+    // Play button
+
+    $(".startbutton").on("click", function () {
+        $(".game").show();
+        $(".gameboard").hide();
+        $(".startbutton").hide();
+        initiateGame();
+    })
+    // Initiation of the Game
+
+    var userPokemon;
+    var enemyPokemon;
+    var userHP;
+    var userAttack;
+    var enemyHP;
+    var enemyDefense;
+    var battling = false;
+
+
+
+    function initiateGame() {
+
+        for (var selectedPokemon of pokemonSet) {
+            
+            // creating a Div for every individual pokemon
+            var pokemon = $("<div>");
+            // giving all pokemon a common class
+            pokemon.addClass("pokemon col-4");
+            // giving each pokemon individual ID
+            pokemon.attr('id', 'pokemon-' + selectedPokemon.name);
+            pokemon.append('<img src=' + selectedPokemon.img + " class='pokemonImage'>"); // adding associated image
+            
+            var stats = $("<div class='statbar row'>");
+            stats.append("<span class='pokeAttack stat col-4'> Atk:" + selectedPokemon.attack + "</spam>")
+            stats.append("<span class='pokeName stat col-4'>" + selectedPokemon.name + "</spam>");
+            stats.append("<span class='pokeHP stat col-4'> HP: " + selectedPokemon.hp + "</spam>")
+
+            stats.appendTo(pokemon);
+
+            pokemon.appendTo('.allPoke'); // adding to after instructions
+
+        }
+    }
+
+    // 2. Select Opponent
+    // 3. When clicking on "attack," attach will double and my HP will go down with their attach
+    // 4. WIN - opponent will leave the screen 
+    // 4A. Player selects new opponent
+    // 5. LOSE - game is reset
 
 });
