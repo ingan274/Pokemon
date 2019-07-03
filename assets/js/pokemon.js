a825777eed4e97c8854772d372472f024e6208d9
ingan <isabelngan27@gmail.com>
2019-07-03 16:40:15 -0700

    everything is ready and except the reset bottom

--- assets/js/pokemon.js
+++ assets/js/pokemon.js
@@ -4,7 +4,8 @@
 // 3. When clicking on "attack," attach will increase and my HP will go down with their defense
 // 4. WIN - opponent will leave the screen 
 // 4A. Player selects new opponent
-// 5. LOSE - game is reset
+// 4B. LOSE - show reset
+// 5. Reset
 
 $(document).ready(function () {
     // 1. Select Pokemon
@@ -16,8 +17,8 @@ $(document).ready(function () {
             idnum: 0,
             ogHp: 70,
             hp: 70,
-            attack: 20,
-            defense: 33,
+            attack: 2,
+            defense: 15,
             img: './assets/img/charmander.png'
         },
         {
@@ -25,8 +26,8 @@ $(document).ready(function () {
             idnum: 1,
             ogHp: 90,
             hp: 90,
-            attack: 40,
-            defense: 30,
+            attack: 4,
+            defense: 15,
             img: './assets/img/dugtrio.png'
         },
         {
@@ -34,8 +35,8 @@ $(document).ready(function () {
             idnum: 2,
             ogHp: 60,
             hp: 60,
-            attack: 25,
-            defense: 20,
+            attack: 2,
+            defense: 10,
             img: './assets/img/eve.png'
         },
         {
@@ -43,8 +44,8 @@ $(document).ready(function () {
             idnum: 3,
             ogHp: 100,
             hp: 100,
-            attack: 30,
-            defense: 20,
+            attack: 3,
+            defense: 10,
             img: './assets/img/persian.png'
         },
         {
@@ -52,8 +53,8 @@ $(document).ready(function () {
             idnum: 4,
             ogHp: 70,
             hp: 70,
-            attack: 30,
-            defense: 40,
+            attack: 3,
+            defense: 20,
             img: './assets/img/pikachu.png'
         },
         {
@@ -61,8 +62,8 @@ $(document).ready(function () {
             idnum: 5,
             ogHp: 130,
             hp: 130,
-            attack: 70,
-            defense: 35,
+            attack: 7,
+            defense: 17,
             img: './assets/img/wailmer.png'
         },
     ]
@@ -73,19 +74,31 @@ $(document).ready(function () {
         $(".game").show();
         $(".gameboard").hide();
         $(".startbutton").hide();
+        $(".head").css("position", "fixed");
+        $(".head").css("left", "0");
+        $(".head").css("bottom", "0");
+        $(".head").css("font-size", "11px");
+        $(".head").css("width", "280px");
+        $(".head").css("margin", "5px 5px 5px 5px");
+        $(".head").css("padding", "5px 5px 5px 5px");
         initiateGame();
     })
     // Initiation of the Game
 
     var userPokemon;
+    var userPokemonName;
     var userHP;
     var userAttack;
 
     var enemyPokemon;
+    var enemyPokemonName;
     var enemyHP;
     var enemyDefense;
+    var enemiesLeft = 5;
 
     var attackButton;
+    var resetButton;
+    var PlayAgainButton;
 
     var remainingPokemon;
 
@@ -105,9 +118,9 @@ $(document).ready(function () {
             pokemon.append('<img src=' + selectedPokemon.img + " class='pokemonImage selectedPokemon' idNum=" + selectedPokemon.idnum + ">"); // adding associated image
 
             var stats = $("<div class='statbar row'>");
-            stats.append("<span class='pokeAttack stat col-4'> Atk:" + selectedPokemon.attack + "</spam>")
-            stats.append("<span class='pokeName stat col-4'>" + selectedPokemon.name + "</spam>");
-            stats.append("<span class='pokeHP stat col-4'> HP:" + selectedPokemon.hp + "</spam>")
+            stats.append("<span class='pokeAttack stat col-3'> Atk:" + selectedPokemon.attack + "</spam>")
+            stats.append("<span class='pokeName stat col-6'>" + selectedPokemon.name + "</spam>");
+            stats.append("<span class='pokeHP stat col-3'> HP:" + selectedPokemon.hp + "</spam>")
 
             stats.appendTo(pokemon);
 
@@ -115,10 +128,11 @@ $(document).ready(function () {
         };
     };
 
-    // Choosing Click Function
-    $(".individualpokemon").on("click touchstart", function () {
+    // Choosing User Pokemon Click Function
+    $(".allPoke").on("click touchstart", ".individualpokemon", function () {
         // assigning correct HP and Attack
         userPokemon = parseInt($(this).attr("idNum"));
+        userPokemonName = pokemonSet[userPokemon].name;
         userHP = pokemonSet[userPokemon].hp;
         userAttack = pokemonSet[userPokemon].attack;
 
@@ -145,32 +159,104 @@ $(document).ready(function () {
     });
 
     // 2. Select Current Opponent
-    $(".enemyDiv").on("click touchstart", function () {
-        // if (!battling) {
-        // set up Hp and and Defense
-        enemyPokemon = parseInt($(this).attr("idNum"));
-        enemyHP = characters[enemyPokemon].hp;
-        enemyDefense = characters[enemyPokemon].defense;
+    $("#remainingcards").on("click touchstart", ".enemyDiv", function () {
+        if (!battling) {
+            // set up Hp and and Defense
+            enemyPokemon = parseInt($(this).attr("idNum"));
+            enemyPokemonName = pokemonSet[enemyPokemon].name;
+            enemyHP = pokemonSet[enemyPokemon].hp;
+            enemyDefense = pokemonSet[enemyPokemon].defense;
 
-        var enemyDiv = $("div[idNum=" + enemyPokemon + "]");
-        enemyDiv.attr("class", "enemyDiv enemyPoke current-enemy"); //adds current-enemy class
-        //move Enemy to Battle area
-        $(enemyDiv).detach().appendTo("#enemycard");
-        battling = true;
+            var enemyDiv = $("div[idNum=" + enemyPokemon + "]");
+            enemyDiv.attr("class", "enemyDiv enemyPoke current-enemy"); //adds current-enemy class
+            //move Enemy to Battle area
+            $(enemyDiv).detach().appendTo("#enemycard");
+            battling = true;
 
-        $(".action").hide();
-        attackButton = $("<button>Attack</button>");
-        attackButton.addClass("attackbutton")
-        attackButton.appendTo(".current-enemy")
+            $(".action").hide();
+            attackButton = $("<button>Attack</button>");
+            attackButton.addClass("attackbutton actionbutton")
+            attackButton.appendTo(".current-enemy")
+
+            resetButton = $("<button>Play Again</button>");
+            resetButton.addClass("resetbutton actionbutton")
+            resetButton.appendTo(".current-enemy")
+            $(".resetbutton").hide();
 
 
-        // }
+        }
     });
 
     // 3. When clicking on "attack," attach will double and my HP will go down with their attach
-    // 4. WIN - opponent will leave the screen 
-    // 4A. Player selects new opponent
-    // 5. LOSE - game is reset
+    $("#enemycard").on("click touchstart", ".attackbutton", function () {
+        if (battling) {
+            userHP -= enemyDefense;
+            enemyHP -= userAttack;
+            userAttack = userAttack + pokemonSet[enemyPokemon].ogHp;
+            updateStats();
+            progressReport();
+        };
+    });
+
+    function updateStats() {
+        // changing stats
+        $("#playercard .currentPoke .statbar .pokeAttack").text("Atk:" + userAttack);
+        $("#playercard .currentPoke .statbar .pokeHP").text("HP:" + userHP);
+        $("#enemycard .enemyPoke .statbar .pokeHP").text("HP:" + enemyHP);
+
+        // adding info
+        $("#battleinfo").html("You took " + enemyDefense + " damage, while your opponent took " + userAttack + " damage. <br/> Your current HP: " + userHP + "<br/> Opponents current HP: " + enemyHP);
+
+    }
+
+    // 4. WIN/LOSE BATTLE - opponent will leave the screen 
+    function progressReport() {
+        if (userHP <= 0) { // if player HP is 0 or below
+            $(".attackbutton").hide(); // hide attack button
+            $("#battleinfo").text("Oh No! Looks like " + userPokemonName + " has fainted."); 
+            $(".resetbutton").show(); //unhide reset button
+        }
+        else if (enemyHP <= 0) { // if enemy HP is 0 or below
+            $(".current-enemy").remove(); 
+            battling = false;
+            enemiesLeft--; //reduce counter of enemies remaining (internal counter)
+
+            if (enemiesLeft === 0) { //if no enemies remain
+                $("#battleinfo").text("You have won all your battles! Congradulations!"); 
+                PlayAgainButton = $("<button>Play Again</button>");
+                PlayAgainButton.addClass("resetbutton actionbutton")
+                PlayAgainButton.appendTo("#enemycard")
+                
+            }
+            else { //if there are still enemies left
+                //display won battle message
+                $("#battleinfo").text("You have defeated " + enemyPokemonName + "! Please select a new opponent.");
+            }
+        }
+    }
+
+    // 5. Reset
+    $("#enemycard").on("click touchstart", ".resetbutton", function () {
+       //reset all variables
+		playerHp = pokemonSet[userPokemon].ogHp;
+		playerAttack = null;
+		enemyHp = pokemonSet[enemyPokemon].ogHp;
+		enemyAttack = null;
+		playerId = null;
+		enemyId = null;
+		enemiesLeft = pokemonSet.length-1;
+        battling = false;
+
+        $(".game").hide();
+        $(".gameboard").hide();
+        $(".startbutton").show();
+		$(".userDiv").remove(); //remove player div
+		$(".current-enemy").remove();//remove current enemy div
+		$(".battleinfo").text(""); //reset battle text
+
+        
+        initiateGame();
+    });
 
 
 });
